335ff7ff4fc1d2121541e83ca4838b4d105ac9f0
Joanna Lin <linjoanna31@gmail.com>
2022-05-31 19:26:50 +1000

    Added week 1 content

---
+++ tut01/face.c
@@ -0,0 +1,17 @@
+// A program that prints the following face
+// ~ ~
+// 0 0
+//  o 
+// \_/
+// T17A, May 2022
+
+#include <stdio.h>
+
+int main(void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+    
+    return 0;
+}
