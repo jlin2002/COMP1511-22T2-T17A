d5fb03d2d2b18c1a470eeb92f9c90db5875695ea
Joanna Lin <linjoanna31@gmail.com>
2022-07-27 14:17:14 +1000

    Added week 9 content

---
+++ tut09/use_after_free.c
@@ -0,0 +1,35 @@
+// Demo of undefined behaviour when freed memory is used
+// T17A, Joanna
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *create_node(int data);
+
+int main(void) {
+    struct node *node1 = create_node(1);
+    free(node1);
+    struct node *node2 = create_node(2);
+
+    // !!! DANGER ZONE - USE AFTER FREE !!!
+    node1->data = 3;
+    
+    // node2 contains 2... or does it?
+    printf("node1 stores: %d\n", node1->data);
+    printf("node2 stores: %d\n", node2->data);
+
+    return 0;
+}
+
+// Creates a node out of the given data
+struct node *create_node(int data) {
+    struct node *new_node = malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
